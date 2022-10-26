13bbb41cd616ba4547a311c55b247cd77b85d3f0
Hakimullah Hakimi <111474773+suvuu@users.noreply.github.com>
2022-10-26 10:00:23 -0400

    linear interpolation
    html css javascript

---
+++ segment/index.js
@@ -0,0 +1,126 @@
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerHeight;
+
+// points, want to see if segments {A,B} and {C,D} intersect
+const A = { x: 200, y: 150 };
+const B = { x: 150, y: 250 };
+const C = { x: 300, y: 500 };
+const D = { x: 550, y: 500 };
+
+const ctx = myCanvas.getContext("2d");
+
+let angle = 0;
+
+// mouse cursor spawns 0,0 at first but changes by onmousemove event
+const mouse = { x: 0, y: 0 };
+
+document.onmousemove = (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+};
+
+let t = -1;
+
+
+animate();
+
+function animate() {
+  const radius = 50;
+
+  // moving A,B points with mouse
+  //
+  A.x = mouse.x + Math.cos(angle) * radius;
+  A.y = mouse.y - Math.sin(angle) * radius;
+  B.x = mouse.x - Math.cos(angle) * radius;
+  B.y = mouse.y + Math.sin(angle) * radius;
+  angle += 0.01;
+
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+  ctx.beginPath();
+
+  ctx.moveTo(A.x, A.y);
+  ctx.lineTo(B.x, B.y);
+  ctx.moveTo(C.x, C.y);
+  ctx.lineTo(D.x, D.y);
+
+  ctx.stroke();
+
+  drawDot(A, "A");
+  drawDot(B, "B");
+  drawDot(C, "C");
+  drawDot(D, "D");
+
+  // lerp gets 3 values (a,b) and a percentage (t). (t) says how far we are from (a) to (b).
+
+  // when t is 0
+  // -----------------
+  // A+(B-A)*t  ->  A+(B-A)*0  ->  A+(0-0)*0  ->  A
+  //
+
+  // when t is 1
+  // -----------------
+  // A+(B-A)*t  ->  A+(B-A)*1  ->  0+(B-0)*1  ->  B
+
+  // TO GET THE MIDDLE POINT OF A LINE SEGMENT
+  // you have to call lerp twice once for the x and once for the y
+  // and make sure (t) value is the same
+
+  // const t=0.5;
+  //const M = {
+  //  x: lerp(A.x, B.x, t),
+ //   y: lerp(A.y, B.y, t),
+ //// };
+  //const N = {
+ //  x: lerp(C.x, D.x, t),
+ //  y: lerp(C.y, D.y, t),
+ // };
+
+  // middle of line segment A(a) & B(b) with M(m).
+
+  // drawDot(M, "M", t < 0 || t > 1);
+  //drawDot(N, "N", t < 0 || t > 1);
+
+  const I = getIntersection(A, B, C, D);
+  if (I) {
+    drawDot(I, "I");
+  }
+
+  requestAnimationFrame(animate);
+}
+
+function getIntersection(A, B, C, D) {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t,
+      };
+    }
+  }
+  return null;
+}
+
+function lerp(A, B, t) {
+  return A + (B - A) * t;
+}
+// vizualize
+function drawDot(point, label) {
+  ctx.beginPath();
+  ctx.fillStyle =  "orange";
+  ctx.arc(point.x, point.y, 10, 0, Math.PI * 2);
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.fillStyle = "blue";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.font = "bold 14px Arial";
+  ctx.fillText(label, point.x, point.y);
+}
